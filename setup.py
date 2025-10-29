aa4d0790f20d9135651592c6075756dfd5d19ca1
Vedantk2004 <vedantkhapekar16@gmail.com>
2025-10-29 21:35:32 +0530

    env created and requirements downloaded and the mongodb credentials are ready to use

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vedant Khapekar",
+    author_email="khapekarvedant@gmail.com",
+    packages=find_packages(),
+)
