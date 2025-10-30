bf356468cf2b31035af14d7221adaa9125a108f9
Vedantk2004 <vedantkhapekar16@gmail.com>
2025-10-30 23:12:20 +0530

    added the data ingestion and related code in the project

--- demo.py
+++ demo.py
@@ -7,12 +7,22 @@
 # logging.error("This is an error message.")
 # logging.critical("This is a critical message.")
 
-from src.logger import logging
-from src.exception import MyException
-import sys
+# --------------------------------------------------------------------------------
 
-try:
-    a = 1 + "Z"
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+# --------------------------------------------------------------------------------
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
